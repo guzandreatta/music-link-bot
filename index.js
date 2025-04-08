44651f8ca5c5bbe8ed6ef6bc4f52ab9595bfcd6e
Guzman Andreatta <guzandreatta@gmail.com>
2025-04-08 15:41:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
-app.message(async ({ message, client, say }) => {
+app.message(async ({ message, client }) => {
+  console.log("Mensaje recibido:", message);
+
   if (message.subtype === 'bot_message') return;
-  if (message.channel && message.channel !== process.env.MUSIC_CHANNEL_ID) return;
+  if (message.channel && message.channel !== process.env.MUSIC_CHANNEL_ID) {
+    console.log("Mensaje ignorado por canal:", message.channel);
+    return;
+  }
+
   await handleMusicLink(message, client);
 });
 
