8899bf8d15521cedb4669e9e4da64a8e3fad5581
Guzman Andreatta <guzandreatta@gmail.com>
2025-04-08 17:31:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = new App({
 app.message(async ({ message, client }) => {
   console.log("Mensaje recibido:", message);
 
-if (['bot_message', 'message_changed', 'message_deleted'].includes(message.subtype)) return;
+if (message.subtype === 'bot_message' || message.subtype === 'message_deleted') return;
 
   await handleMusicLink(message, client);
 });
